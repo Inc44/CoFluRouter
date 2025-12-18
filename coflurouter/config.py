70296d194124fc7313a4ecf80ca09045f995d4ff
Inc <121856799+Inc44@users.noreply.github.com>
2025-12-18 23:49:13 +0100

    bugfix

--- coflurouter/config.py
+++ coflurouter/config.py
@@ -6,8 +6,8 @@ import ast
 
 class Config:
 	def __init__(self, args: argparse.Namespace):
-		self.accessibility: List[Tuple[int, int, int]] = ast.literal_eval(
-			f"[{args.accessibility}]"
+		self.accessibility: List[Tuple[int, int, int]] = (
+			ast.literal_eval(f"[{args.accessibility}]") if args.accessibility else []
 		)
 		self.fetch: bool = args.fetch
 		self.update: bool = args.update
